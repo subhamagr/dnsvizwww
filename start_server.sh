833fe9ef8984e02285694fe679ec5c0ec773bc6b
Subham <subhamagr@users.noreply.github.com>
2022-07-25 11:06:26 +0530

    Perform db migration on container startup

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python3 /deploy/app/manage.py migrate --run-syncdb
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     python3 /deploy/app/manage.py createsuperuser --no-input
 fi
